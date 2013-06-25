f93c164661ae14396dd867d7eae83f3f8b1846d4
Guido Serra aka Zeph <guido.serra@rocket-internet.de>
2013-06-25 14:48:53 +0200

    better working setup, just the PuppetLabse packages

--- epel_puppet.sh
+++ epel_puppet.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
-# ref: http://www.rackspace.com/knowledge_center/article/installing-rhel-epel-repo-on-centos-5x-or-6x
+
+# skip if unnecessary...
+rpm -qi puppetlabs-release-6-7 && exit 0
+# http://docs.puppetlabs.com/guides/puppetlabs_package_repositories.html
+rpm -ivh http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm
 
 # skip if unnecessary...
 rpm -qi puppet && exit 0
-
-# let's get the repo ref packages first
-wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-wget http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
-sudo rpm -Uvh remi-release-6*.rpm epel-release-6*.rpm
-
-# then some simlinking
-ls -1 /etc/yum.repos.d/epel* /etc/yum.repos.d/remi.repo
-
 # finally...
 yum -y install puppet
