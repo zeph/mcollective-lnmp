b0bb2b83d36343ffae52d6a4077d996b5a14cab2
Guido Serra aka Zeph <guido.serra@rocket-internet.de>
2013-06-26 15:25:15 +0200

    hack to get erlang & rabbitmq

--- manifests/repo/rhel.pp
+++ manifests/repo/rhel.pp
@@ -1,5 +1,6 @@
 class rabbitmq::repo::rhel (
     $key = "http://www.rabbitmq.com/rabbitmq-signing-key-public.asc",
+    $erepo = "http://repos.fedorapeople.org/repos/peter/erlang/epel-erlang.repo",
     $version = "2.8.4",
     $relversion = "1",
 ) { 
@@ -11,7 +12,18 @@ class rabbitmq::repo::rhel (
         provider => rpm,
         ensure => installed,
         source => "http://www.rabbitmq.com/releases/rabbitmq-server/v${version}/rabbitmq-server-${version}-${relversion}.noarch.rpm",
-        require => Exec["rpm --import ${key}"],
+        require => [Exec["rpm --import ${key}"], Package["erlang"]],
+    }
+
+    # wget -O /etc/yum.repos.d/epel-erlang.repo http://repos.fedorapeople.org/repos/peter/erlang/epel-erlang.repo && yum install erlang
+    package { "erlang":
+        provider => rpm,
+        ensure => installed,
+        require => Exec["wget -O /etc/yum.repos.d/epel-erlang.repo ${erepo}"],
+    }
+
+    exec { "wget -O /etc/yum.repos.d/epel-erlang.repo ${erepo}":
+        path => ["/bin","/usr/bin","/sbin","/usr/sbin"],
     }
 
 }
