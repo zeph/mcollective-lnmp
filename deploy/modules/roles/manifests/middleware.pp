25b0caaea3e321fad502fad088e10f3b34c12c62
Guido Serra aka Zeph <guido.serra@rocket-internet.de>
2013-07-04 15:57:55 +0200

    dependency requirement

--- deploy/modules/roles/manifests/middleware.pp
+++ deploy/modules/roles/manifests/middleware.pp
@@ -2,13 +2,6 @@ class roles::middleware {
   service{"iptables": ensure => stopped}
 
   class{"repos": } ->
-
-  class{ 'rabbitmq::server':
-    version      => "3.1.2-1",
-    plugins      => ['rabbitmq_stomp', 'rabbitmq_management'],
-    config_stomp => true,
-  } -> 
-
   class{"puppet": } ->
   class{"nagios": } ->
   class{"puppet::master": } ->
@@ -16,33 +9,42 @@ class roles::middleware {
   class{"motd": } ->
   Class[$name]
 
+  class{'rabbitmq::server':
+    version      => "3.1.2",
+    plugins      => ['rabbitmq_stomp', 'rabbitmq_management'],
+    config_stomp => true,
+  } 
+
   rabbitmq_vhost { '/mcollective':
     ensure => present,
     provider => 'rabbitmqctl',
-  }
+  } 
   rabbitmq_user { 'mcollective':
     admin    => true,
     password => 'changeme',
     provider => 'rabbitmqctl',
-  }
+  } 
+
   rabbitmq_user_permissions { 'mcollective@/mcollective':
     configure_permission => '.*',
     read_permission      => '.*',
     write_permission     => '.*',
-  }
+    require  => [Rabbitmq_vhost['/mcollective'], Rabbitmq_user['mcollective']],
+  } 
 
   rabbitmq_exchange { 'mcollective_broadcast@/mcollective':
     type     => 'topic',
     provider => 'rabbitmqadmin',
     user     => 'mcollective',
     password => 'changeme',
-    require  => Rabbitmq_user['mcollective'],
-  } 
+    require  => Rabbitmq_user_permissions['mcollective@/mcollective'],
+  }  
+
   rabbitmq_exchange { 'mcollective_directed@/mcollective':
     type => 'direct',
     provider => 'rabbitmqadmin',
     user => 'mcollective',
     password => 'changeme',
-    require  => Rabbitmq_user['mcollective'],
+    require  => Rabbitmq_user_permissions['mcollective@/mcollective'],
   } 
 }
