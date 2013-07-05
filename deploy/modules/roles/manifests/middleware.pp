f09d67cafd2feaa37583f9bccd0c4d1e5ebee4bc
Guido Serra aka Zeph <guido.serra@rocket-internet.de>
2013-07-05 12:42:20 +0200

    seq. is right but... This is just the default provider for rabbitmq_user

--- deploy/modules/roles/manifests/middleware.pp
+++ deploy/modules/roles/manifests/middleware.pp
@@ -11,40 +11,39 @@ class roles::middleware {
 
   class{'rabbitmq::server':
     version      => "3.1.2",
-    plugins      => ['rabbitmq_stomp', 'rabbitmq_management'],
     config_stomp => true,
   } 
 
-  rabbitmq_vhost { '/mcollective':
+  $plugins  = ['rabbitmq_stomp', 'rabbitmq_management']
+
+  rabbitmq_plugin { $plugins:
     ensure => present,
-    provider => 'rabbitmqctl',
+    provider => 'rabbitmqplugins',
   } 
   rabbitmq_user { 'mcollective':
     admin    => true,
     password => 'changeme',
     provider => 'rabbitmqctl',
   } 
-
+  rabbitmq_vhost { '/mcollective':
+    ensure => present,
+    provider => 'rabbitmqctl',
+  } 
   rabbitmq_user_permissions { 'mcollective@/mcollective':
     configure_permission => '.*',
     read_permission      => '.*',
     write_permission     => '.*',
-    require  => [Rabbitmq_vhost['/mcollective'], Rabbitmq_user['mcollective']],
   } 
-
   rabbitmq_exchange { 'mcollective_broadcast@/mcollective':
     type     => 'topic',
     provider => 'rabbitmqadmin',
     user     => 'mcollective',
     password => 'changeme',
-    require  => Rabbitmq_user_permissions['mcollective@/mcollective'],
   }  
-
   rabbitmq_exchange { 'mcollective_directed@/mcollective':
     type => 'direct',
     provider => 'rabbitmqadmin',
     user => 'mcollective',
     password => 'changeme',
-    require  => Rabbitmq_user_permissions['mcollective@/mcollective'],
   } 
 }
