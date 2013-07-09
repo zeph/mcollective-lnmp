16e466af2a64371dc048abc7ee3becd2ae9a0565
Guido Serra aka Zeph <guido.serra@rocket-internet.de>
2013-07-09 13:26:46 +0200

    basics to try to match Debian naming convention

--- deploy/modules/mcollective/manifests/plugin.pp
+++ deploy/modules/mcollective/manifests/plugin.pp
@@ -1,42 +1,47 @@
 define mcollective::plugin (
-    $type,                  # agent, facts etc
-    $server = true,         # install the server side plugins
-    $client = true,         # install the client side plugins
-    $ensure = present,
-    $config = {},           # keys to configure
-    $method = "package"     # package or a url prefix
+  $type,                  # agent, facts etc
+  $server = true,         # install the server side plugins
+  $client = true,         # install the client side plugins
+  $ensure = present,
+  $config = {},           # keys to configure
+  $method = "package"     # package or a url prefix
 ) {
-    # TODO: support file base plugin distribution
-    # TODO: properly support ensure
+  # TODO: support file base plugin distribution
+  # TODO: properly support ensure
 
-    case $type {
-        default: {
-                    $client_packages = ["mcollective-${name}-client"]
-                    $common_packages = ["mcollective-${name}-common"]
-                    $server_packages = ["mcollective-${name}-${type}"]
-                 }
+  case $type {
+    default: {
+      $client_packages = ["mcollective-${name}-client"]
+      $common_packages = ["mcollective-${name}-common"]
+      $server_packages = ["mcollective-${name}-${type}"]
     }
+  }
 
-    Package {
-        ensure => $ensure,
-        require => Class["mcollective::install"],
-        notify => Class["mcollective::service"]
-    }
+  Package {
+    ensure => $ensure,
+    require => Class["mcollective::install"],
+    notify => Class["mcollective::service"]
+  }
 
+  if $::osfamily == 'Debian' {
+    package {"mcollective-plugins-${name}": }
+
+  } else {
     package{$common_packages: }
 
     if $server {
-        package{$server_packages: }
+      package{$server_packages: }
     }
 
     if $client {
-        package{$client_packages: }
+      package{$client_packages: }
     }
 
     if !empty($config) {
-        mcollective::plugin_config{$name:
-            options => $config,
-            ensure  => $ensure,
-        }
+      mcollective::plugin_config{$name:
+        options => $config,
+        ensure  => $ensure,
+      }
     }
+  }
 }
