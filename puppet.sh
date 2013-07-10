caac41cb9bc2c7117d3ba9cd97121db6162e94d5
Guido Serra aka Zeph <guido.serra@rocket-internet.de>
2013-07-10 12:09:02 +0200

    Debian/Ubuntu does not seem to like the redirects

--- puppet.sh
+++ puppet.sh
@@ -3,22 +3,22 @@
 # http://docs.puppetlabs.com/guides/puppetlabs_package_repositories.html
 # http://www.rackspace.com/knowledge_center/article/installing-rhel-epel-repo-on-centos-5x-or-6x
 
-dpkg --version 2>&1> /dev/null
+dpkg --version
 # KISS distro detector ;)
 if [ $? -eq 0 ]; then
 	# skip if unnecessary...
-	dpkg -l puppetlabs-release 2>&1> /dev/null && exit 0
+	dpkg -l puppetlabs-release && exit 0
 	wget http://apt.puppetlabs.com/puppetlabs-release-precise.deb && \
 		dpkg -i puppetlabs-release-precise.deb && \
 		apt-get update && apt-get -y install puppet
 else
 	# skip if unnecessary...
-	rpm -qi puppetlabs-release 2>&1> /dev/null || \
+	rpm -qi puppetlabs-release || \
 	rpm -ivh http://yum.puppetlabs.com/el/6/products/i386/puppetlabs-release-6-7.noarch.rpm && \
 		yum -y install puppet
-	rpm -qi epel-release 2>&1> /dev/null || \
+	rpm -qi epel-release || \
 	    rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-	rpm -qi remi-release 2>&1> /dev/null || \
+	rpm -qi remi-release || \
 		rpm -ivh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm && \
 		ls -1 /etc/yum.repos.d/epel* /etc/yum.repos.d/remi.repo
 fi
